0fb5e214a1ecd7da815f262b24926029abcdd43a
Jiwon <jiwon1.han@gmail.com>
2020-08-10 20:29:21 -0700

    working on changing height when toggling

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-  $('#carousel').carousel({
-      interval: 4500
-  })
-});    
+// $(document).ready(function() {
+//   $('#carousel').carousel({
+//       interval: 4500
+//   })
+ 
+// });    
+
+// $(document).ready(() => {
+//   console.log('hello');
+//   $("#collapseSeoul").on("show.bs.collapse", (e) => {
+//     console.log('hi');
+//   })
+// });
---
+++ js/toggle.js
@@ -0,0 +1,8 @@
+
+ $(document).ready(function() {
+  console.log('hello'); 
+  $("#collapseSeoul").on("show.bs.collapse", (e) => {
+        console.log('hi');
+      })
+ })
+ 
