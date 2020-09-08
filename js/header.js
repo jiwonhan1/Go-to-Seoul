e86d4743992221a151877924ace2d33e9d943eda
Jiwon <jiwon1.han@gmail.com>
2020-09-07 18:28:27 -0700

    changed hamburger from link to button for better UI display

--- js/header.js
+++ js/header.js
@@ -4,4 +4,21 @@ const menu = document.querySelector('#navbar__list');
 
 toggleBtn.addEventListener('click', () => {
 menu.classList.toggle('active');
-})
+menu.classList.remove('clicked')
+});
+
+const links = document.querySelectorAll('.menu__link');
+console.log(links);
+
+links.forEach(link => { link.addEventListener('click', () => { menu.classList.add('clicked')})});
+// const links = document.getElementsByClassName('menu__link');
+
+// console.log(links);
+// links.forEach(link => { 
+//   link.addEventListener('click', () => {menu.stlye.display = 'none'})
+// });
+
+// .forEach(link => { link.addEventListener('click', event => {
+//   menu.style.display = 'none';
+// })
+// });
