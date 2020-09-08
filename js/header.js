58920d2cce3c002cb304f5385048dd346c06cff1
Jiwon <jiwon1.han@gmail.com>
2020-09-07 18:32:48 -0700

    adjusted nav landing to section

--- js/header.js
+++ js/header.js
@@ -11,14 +11,3 @@ const links = document.querySelectorAll('.menu__link');
 console.log(links);
 
 links.forEach(link => { link.addEventListener('click', () => { menu.classList.add('clicked')})});
-// const links = document.getElementsByClassName('menu__link');
-
-// console.log(links);
-// links.forEach(link => { 
-//   link.addEventListener('click', () => {menu.stlye.display = 'none'})
-// });
-
-// .forEach(link => { link.addEventListener('click', event => {
-//   menu.style.display = 'none';
-// })
-// });
